add6c2521a21494bd2546893b9ef4a9a59e560b9
Maarten <maartenwachtersmast@gmail.com>
2017-06-02 13:12:31 +0200

    verandering tonen

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyparser.json());
 		console.log("Server wordt afgesloten");
 		process.exit(0);
 	});
-
+//Mr Luyts
 	app.use(function(req, res, next) {
 	  	res.header("Access-Control-Allow-Origin", "*");
 	  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
